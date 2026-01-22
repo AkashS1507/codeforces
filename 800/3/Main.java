6795a016b29d0ea0bd2abb821bce3513bbceebf2
AkashS1507 <akashsahu15072004@gmail.com>
2026-01-22 15:38:41 +0530

    3 & 4

--- 800/3/Main.java
+++ 800/3/Main.java
@@ -3,6 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            int dots = 0;
+            int maxSegment = 0;
+            int current = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '.') {
+                    dots++;
+                    current++;
+                    maxSegment = Math.max(maxSegment, current);
+                } else {
+                    current = 0;
+                }
+            }
+
+            if (maxSegment > 2) {
+                System.out.println(2);
+            } else {
+                System.out.println(dots);
+            }
+        }
 
         sc.close();
     }
