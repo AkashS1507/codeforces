366f61008147c56fe517924a0a903440cbfada39
AkashS1507 <akashsahu15072004@gmail.com>
2026-01-25 17:37:14 +0530

    8

---
+++ 800/8/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                list.add(sc.nextInt());
+            }
+            if(list.contains(k)){
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("No");
+            }
+        }
+        sc.close();
+    }
+}
