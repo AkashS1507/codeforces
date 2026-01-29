83e7d13906ef0fa6f7d1e977d45a11b1b857ec63
AkashS1507 <akashsahu15072004@gmail.com>
2026-01-29 15:46:38 +0530

    11

---
+++ 800/11/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int min = Integer.MAX_VALUE;
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+            min = Math.min(Math.abs(a[i]), min);
+        }
+        System.out.println((min == 0)? 0 : Math.abs(min));
+        sc.close();
+    }
+}
